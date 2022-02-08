4e618e4416565846b50ce307ada4b52cb4084280
OVicio10 <joao.guilherme100@hotmail.com>
2022-02-08 14:33:47 -0300

    Projeto final pronto
    controle_gastos - att
    contas - att
    venv - att

--- Django 2.0/contas/views.py
+++ Django 2.0/contas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transacao
+from .form import TransacaoForm
 from django.http import HttpResponse
 import datetime
 
@@ -14,3 +15,33 @@ def listagem(request):
     data['transacoes'] = Transacao.objects.all()
 
     return render(request, 'contas/listagem.html', data)
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    return render(request,'contas/form.html', data)
+
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    data['transacao'] = transacao
+    return render(request, 'contas/form.html', data)
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_listagem')
